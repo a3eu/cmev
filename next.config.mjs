efbd3836a93521b5d8bc42bd9af312dad505de5e
a3eu <azgh@nenaro.com>
2025-08-26 13:10:38 -0700

    import v2

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'dist',
+  basePath: process.env.NODE_ENV === 'production' ? '/cmev-v0' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/cmev-v0' : '',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
