33e97841fe77f587ea356d5507f56ad3401d10bb
a3eu <azgh@nenaro.com>
2025-08-31 04:15:46 +0000

    update buy tickets button to link to zeffy ticketing page
    Co-Authored-By: sketch <hello@sketch.dev>
    Change-ID: s889d7a94f92b14e1k

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function HomePage() {
             <Button 
               size="lg"
               className="bg-red-600 hover:bg-red-700 text-white font-bold px-12 py-6 text-xl"
-              onClick={() => window.open("#events", "_self")}
+              onClick={() => window.open("https://www.zeffy.com/en-US/ticketing/tango-y-mas", "_blank")}
             >
               BUY TICKETS
             </Button>
